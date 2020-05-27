43e185c16f487add46039501b6c7c889ee027511
cuishuai <18210301859@163.com>
2020-05-27 17:41:21 +0800

    eureka

---
+++ crx-service-ribbon/src/test/java/com/aisc/serviceribbon/CrxServiceRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisc.serviceribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrxServiceRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
